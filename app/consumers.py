33f62b633f035f60fdbcb1ba8d83cac86375e983
matrix-bro <razmhz11@gmail.com>
2024-05-25 18:31:37 +0545

    Written a basic chat consumer to test websocket connection and message flow

---
+++ app/consumers.py
@@ -0,0 +1,51 @@
+from channels.generic.websocket import AsyncWebsocketConsumer
+import json
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['conversation_id']
+        self.room_group_name = f"chat_{self.room_name}"
+
+        # Join room group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+    
+    async def disconnect(self, code):
+        # Leave room group
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        message = data['message']
+        senderId = data['senderId']
+        recipientId = data['recipientId']
+
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'senderId': senderId,
+                'recipientId': recipientId,
+
+            }
+        )
+    
+    async def chat_message(self, event):
+        message = event['message']
+        senderId = event['senderId']
+        recipientId = event['recipientId']
+
+        await self.send(text_data=json.dumps({
+            'message': message,
+            'senderId': senderId,
+            'recipientId': recipientId,
+        }))
--- app/routing.py
+++ app/routing.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from app.consumers import ChatConsumer
 
 websocket_urlpatterns = [
+    path('ws/chat/<str:conversation_id>/', ChatConsumer.as_asgi()),
 ]
